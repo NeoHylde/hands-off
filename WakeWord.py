31f7ba2dbbce0e633477554c481dc63d368562cd
Neo <neo.hyldelund@gmail.com>
2025-07-29 20:35:59 -0700

    integrated

--- WakeWord.py
+++ WakeWord.py
@@ -7,6 +7,7 @@ import signal
 import sys
 from dotenv import load_dotenv
 from Recorder import Record
+from Music import Music
 
 class Wake:
     def __init__(self):
@@ -17,6 +18,29 @@ class Wake:
         )
         self.recorder = PvRecorder(device_index=-1, frame_length=self.porcupine.frame_length)
         self.whisper = Record()
+        self.music = Music()
+
+    def parse_command(self, transcription):
+        transcription = transcription.lower()
+        
+        if transcription.startswith("play"):
+            song_name = transcription.replace("play", "").strip()
+            self.music.play_track(song_name)
+
+        elif "pause" in transcription:
+            self.music.pause_track()
+
+        elif "next" in transcription:
+            self.music.next_track()
+
+        elif "skip forward" in transcription:
+            self.music.skip_forward(60)
+
+        elif "continue" in transcription:
+            self.music.continue_track()
+
+        return ("unknown", transcription)
+
 
     def start(self):
         print("listening for wake word")
@@ -28,7 +52,8 @@ class Wake:
                 keyword_index = self.porcupine.process(frame)
                 if keyword_index >= 0:
                     print("wake word detected")
-                    self.whisper.start()
+                    transcript = self.whisper.start()
+                    self.parse_command(transcription=transcript)
         except KeyboardInterrupt:
             print("stopping")
         finally:
